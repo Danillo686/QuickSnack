40508b3bac971e1bc6161a68aa3095e8b328f2d9
Joao-Pietro <nick.zin.izze@gmail.com>
2025-11-12 15:18:47 -0300

    Set up navigation and user context in App.js
    Refactored App component to include navigation and user context.

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './screens/Home';
+import Login from './screens/Login';
+import Aluno from './screens/Aluno';
+import Admin from './screens/Admin';
+import { UserProvider } from './context/UserContext';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <UserProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false}} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Aluno" component={Aluno} />
+          <Stack.Screen name="Admin" component={Admin} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
